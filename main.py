a253fc9d936cd41d4fe426dd6a102e7934b05575
Z3R0GT <117790541+Z3R0GT@users.noreply.github.com>
2024-09-05 16:19:24 -0500

    app-0.0.5.3 TEST

--- main.py
+++ main.py
@@ -1,41 +1,28 @@
 from core import *
 
-#DEBUG_MODE[0] = False
+def uwu(ins, var):
+    print(ins, var)
 
-def muertito(*nm):
-    print(nm)
+#a = Menu("uwu", 10, 20, "#")
+#n = Buttons("owo", 1, 1, "hola", action=uwu)
+#a.add_child(n)
 
+m = Mapa("UWU", 100, 20, "#")
+#m.get_pre_view()
+#m.get_meta()
+c = Structure("uwu", 1, 1, 20, 10, "#")
 
-
-a = Menu("uwu", 100, 16, "$","uwu")
-#p = Panel("owo", 17, 1, "#" , 25, 12)
-#a.add_child(p, as_add=False)
-
-#MSG = ["Hola", "Como", "estas", 
-#       "wuwu","Hola", "Como", 
-#       "estas", "wuwu","Hola", 
-#       "aomo", "estas", "wuwu"]
-
-#btn3 = Buttons("third", 60, 13, "mame", default="OUTP", action=muertito)
-#btn3.set_panel_output(0, MSG)
-#a.add_child(btn3)
-#RENDER = 10
-#BTN_OU = [btn3]
-
-#btn1 = Buttons("next", 17, 13, "Siguiente", default="LIST")
-#btn1.set_panel_listener(p, 0)
-#a.add_child(btn1)
-
-#btn2 = Buttons("back", 17+len(btn1.character)+5, 13, "Atras", default="LIST")
-#btn2.set_panel_listener(p, 10)
-#a.add_child(btn2)
-
-#p.set_table_init(btn1, btn2, MSG, 10)
-#p._table_edit()
-#p.table_edit(10, [btn3], "uwu", is_new=True)
-
-#a.adder(p)
+k = Structure("owo", c.vec[0]+c.transform[0]+2, 1, 20, 10, "#")
 
 
-#a.start_cast()
-#a.get_pre_view()
+d = Door("uw", 1, 1, "D", multi=4)
+v = Door("uw", 1, 1, "D", multi=4)
+
+c.add_child(d)
+
+k.add_child(v)
+
+m.add_child(c)
+m.add_child(k)
+
+m.get_pre_view()
